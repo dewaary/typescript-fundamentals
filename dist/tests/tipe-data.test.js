2baaed19027a32bdb526e4b16865cf604ff101d8
dewaary <dewa.septiadi@ptgue.com>
2024-08-27 17:21:26 +0800

    Type Data Primitif

---
+++ dist/tests/tipe-data.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('Data Type', function () {
+    it('should must declare', function () {
+        const name = "Dewa Gede Agung Ari";
+        const balance = 10000000;
+        const isVip = true;
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data Type', function () {
+    it('should must declare', function () {
+        const name: string = "Dewa Gede Agung Ari"
+        const balance: number = 10000000
+        const isVip: boolean = true
+
+        console.info(name)
+        console.info(balance)
+        console.info(isVip)
+    })
+})
